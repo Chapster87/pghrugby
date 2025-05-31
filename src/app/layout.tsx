f5366b873ed60b5867c3865348fd69295a6baffc
Andy Chapman <achapm87@gmail.com>
2025-05-31 16:58:29 -0400

    Implement VisualEditor

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,17 +3,27 @@ import { Metadata } from "next"
 import "styles/globals.css"
 import { Providers } from "./providers"
 
+import { VisualEditing } from "next-sanity"
+import { draftMode } from "next/headers"
+import { DisableDraftMode } from "../components/DisableDraftMode"
+
 export const metadata: Metadata = {
   metadataBase: new URL(getBaseURL()),
 }
 
-export default function RootLayout(props: { children: React.ReactNode }) {
+export default async function RootLayout(props: { children: React.ReactNode }) {
   return (
     <html lang="en" data-mode="light">
       <body>
         <Providers>
           <main className="relative">{props.children}</main>
         </Providers>
+        {(await draftMode()).isEnabled && (
+          <>
+            <VisualEditing />
+            <DisableDraftMode />
+          </>
+        )}
       </body>
     </html>
   )
