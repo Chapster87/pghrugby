199384186e05dd0107a66df47b286eed78b046e6
Andy Chapman <achapm87@gmail.com>
2025-06-05 02:24:12 -0400

    Configure PostCSS

--- postcss.config.js
+++ postcss.config.js
@@ -2,5 +2,20 @@ module.exports = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
+    "postcss-flexbugs-fixes": {},
+    // "@csstools/postcss-global-data": {
+    //   files: ["./src/styles/custom-media.css"],
+    // },
+    "postcss-preset-env": {
+      stage: 3,
+      browsers: ["defaults"],
+      autoprefixer: { flexbox: "no-2009" },
+      features: {
+        "nesting-rules": true,
+        "custom-media-queries": true,
+        "custom-properties": false,
+      },
+    },
+    "postcss-normalize": { browsers: "defaults" },
   },
 }
