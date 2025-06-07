2aef894c1388f442d5fe6847b2a1121a8bf00e60
Andy Chapman <achapm87@gmail.com>
2025-06-06 21:47:56 -0400

    Update tsconfig paths and react version fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import { getBaseURL } from "@lib/util/env"
 import { Metadata } from "next"
-import "styles/globals.css"
+import "@styles/globals.css"
 import { Providers } from "./providers"
 
 import BreakpointIndicator from "@components/BreakpointIndicator"
