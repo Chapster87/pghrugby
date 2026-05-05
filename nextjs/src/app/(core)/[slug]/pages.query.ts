928751d8ab787979173bb0d6f6abf9e00a5547d5
Andy Chapman <achapm87@gmail.com>
2026-05-04 20:31:46 -0400

    prep 'Pages' for strapi

--- nextjs/src/app/(core)/[slug]/pages.query.ts
+++ nextjs/src/app/(core)/[slug]/pages.query.ts
@@ -1,5 +1,6 @@
 import { defineQuery } from "next-sanity"
 import { seoFragment } from "@fragments/seo-fragment"
+import { gql } from "graphql-request"
 
 export const pageQuery = defineQuery(
   `*[_type == "page" && slug.current == $slug][0]{
@@ -28,3 +29,32 @@ export const pagesSlugs = defineQuery(`
   *[_type == "page" && defined(slug.current)]
   {"slug": slug.current}
 `)
+
+export const pageSlugs = gql`
+  query {
+    wpPages(status: PUBLISHED, filters: { page_status: { eq: "publish" } }) {
+      slug
+    }
+  }
+`
+
+export const ABOUT_PAGE_QUERY = gql`
+  query getAboutPage {
+    wpPages(filters: { slug: { eq: "about" } }) {
+      title
+      slug
+      seo {
+        canonicalURL
+        metaDescription
+        metaImage {
+          url
+        }
+      }
+      featured_media {
+        alternativeText
+        url
+      }
+      content
+    }
+  }
+`
